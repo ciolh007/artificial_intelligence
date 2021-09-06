f74578670452bc3dc9f6d56ae55e0f2327d59ca7
Amelia Lustig <hkmoritz10@outlook.com>
2021-09-06 19:54:04 +0800

    deploy Next.js to gh-pages

---
+++ out/_next/static/79g5pRXnvNhqm9yTOpk3i/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-4678ca7dda4ad0fbe525.js"],"/_error":["static/chunks/pages/_error-eb7f79f2af83e8ddf5e2.js"],"/about":["static/chunks/pages/about-f90f9b332f7010d8e33a.js"],sortedPages:["/","/_app","/_error","/about"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
