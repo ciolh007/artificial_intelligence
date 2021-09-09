1bc20eb1c34e4e9ce5c9508f05a14fffc4060b3b
Amelia Lustig <hkmoritz10@outlook.com>
2021-09-09 20:54:26 +0800

    nkust

--- env-config.js
+++ env-config.js
@@ -1,5 +1,5 @@
 const prod = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  'process.env.BACKEND_URL': prod ? '/teaching' : '',
+  'process.env.BACKEND_URL': prod ? '/artificial_intelligence' : '',
 }
